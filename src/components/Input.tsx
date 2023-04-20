ba48f9b18f740b05fbcd5dcbde1348911a94d9ae
Ace <qanhltv11@gmail.com>
2023-04-20 15:33:58 +0700

    Create email, lock screen and reset page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = (props: InputProps): JSX.Element => {
-  return <input {...props} className={`p-2 border-red-200 border-2 ${props.className ?? ''}`} />
+  return <input {...props} className={`flex border-sky-600 rounded-md border-2 mt-2 mb-4 px-16 py-2 outline-none ${props.className ?? ''}`} />
 }
 
 export default Input
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/index'
 import './style.css'
 import Login from './pages/login'
+import Email from './pages/email'
+import LockScreen from './pages/lockscreen'
+import Reset from './pages/reset'
 
 const App = (): JSX.Element => {
   const router = createBrowserRouter([
@@ -13,6 +16,21 @@ const App = (): JSX.Element => {
     {
       path: '/login',
       element: <Login />
+    },
+
+    {
+      path: '/email',
+      element: <Email />
+    },
+
+    {
+      path: '/lockScreen',
+      element: <LockScreen />
+    },
+
+    {
+      path: '/resetPassword',
+      element: <Reset />
     }
   ])
 
