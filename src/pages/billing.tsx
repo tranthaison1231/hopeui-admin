18671a55f4f3a0dd337b64be642ff691df532579
7777 <vermas8900@gmail.com>
2023-04-26 15:08:48 +0700

    done billing page

--- src/pages/billing.tsx
+++ src/pages/billing.tsx
@@ -1,14 +1,16 @@
 import BillingContent from '@/components/BillingContent'
 import { Background } from '@/components/Background'
+import setting from '@/assets/svgs/settings.svg'
 import FooterSpacial from '@/components/FooterSpacial'
 
 
 const Billing = (): JSX.Element => {
   return (
-    <div className="bg-gray-400 h-full relative ">
+    <div className="bg-gray-400 h-[137%] relative ">
       <Background className="" />
       <BillingContent className=""/>
-      <FooterSpacial className='w-full bottom-0'/>
+      <img src={setting} alt="" className='absolute right-0 translate-y-32'/>
+      <FooterSpacial className='w-full absolute bottom-0 '/>
     </div>
   )
 }
