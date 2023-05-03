4b5b2191097724c0494ede37c5802e55dc496b5a
Ace <qanhltv11@gmail.com>
2023-05-03 14:39:27 +0700

    Create Collapse Button

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = ({ className = '' }: Props): JSX.Element => {
   const { isCollapsed } = useContext(NavbarContext)
   return (
     <div className={`${className} flex gap-2`}>
-      <img src={logo} alt="logo" className="w-7 h-7" />
+      <img src={logo} alt="logo" className={`"w-7 h-7" ${!isCollapsed ? '' : 'ml-8'}`} />
       { !isCollapsed && <span className="text-3xl"> Hope Ui </span>}
     </div>
   )
