7772e2a1b17cb2e271426fbf39a228aacdd0d1ed
PhanDong28 <dongpdde150247@fpt.edu.vn>
2023-05-05 18:08:17 +0700

    update navbar hidden

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -12,19 +12,15 @@ interface Props {
 const MenuItem = ({ icon, name, active, onClick }: Props): JSX.Element => {
   const { isCollapsed } = useContext(NavbarContext)
   return (
-    <div className={``}>
-      <div
-        className={`flex flex-row py-2 cursor-pointer ${isCollapsed ? 'w-min ml-12 pl-[18px]' : 'px-5'}  ${
-          active ? 'bg-blue-600 rounded-md w-full' : 'justify-between'
-        }`}
-        onClick={onClick}
-      >
-        {React.cloneElement(icon, { className: active ? 'text-white' : 'text-[#8A92A6]' })}
-        <div className={`px-3 pb-0 text-gray-500 text-base ${active ? 'text-white' : ''}`}>{!isCollapsed && name}</div>
-        {!isCollapsed && !active && (
-          <ArrowRight className={active ? 'text-white' : 'text-[#8A92A6]'} alt="arrow right" />
-        )}
-      </div>
+    <div
+      className={`flex flex-row py-2 cursor-pointer ${isCollapsed ? 'w-min ml-12 pl-[18px]' : 'px-5'}  ${
+        active ? 'bg-blue-600 rounded-md w-full' : 'justify-between'
+      }`}
+      onClick={onClick}
+    >
+      {React.cloneElement(icon, { className: active ? 'text-white' : 'text-[#8A92A6]' })}
+      <div className={`px-3 pb-0 text-gray-500 text-base ${active ? 'text-white' : ''}`}>{!isCollapsed && name}</div>
+      {!isCollapsed && !active && <ArrowRight className={active ? 'text-white' : 'text-[#8A92A6]'} alt="arrow right" />}
     </div>
   )
 }
