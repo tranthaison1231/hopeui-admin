75f843fecded5087096bb69af2d4e380a9a3820e
PhanDong28 <dongpdde150247@fpt.edu.vn>
2023-05-08 18:43:51 +0700

    add maintsx

---
+++ src/main.tsx
@@ -0,0 +1,76 @@
+import NotFoundPage from '@/pages/404'
+import ServerErrorPage from '@/pages/500'
+import Email from '@/pages/email'
+import Home from '@/pages/index'
+import LockScreen from '@/pages/lock-screen'
+import Login from '@/pages/login'
+import ResetPassword from '@/pages/reset-password'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Maintenance from './pages/maintenance'
+import SignUp from './pages/sign-up'
+import './style.css'
+import Billing from './pages/billing'
+import MenuStyle from './pages/menu-style'
+import { NavbarProvider } from './contexts/NavbarContext'
+
+const App = (): JSX.Element => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/menu-style',
+      element: <MenuStyle />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/sign-up',
+      element: <SignUp />
+    },
+    {
+      path: '/email',
+      element: <Email />
+    },
+    {
+      path: '/lock-screen',
+      element: <LockScreen />
+    },
+    {
+      path: '/reset-password',
+      element: <ResetPassword />
+    },
+    {
+      path: '/maintenance',
+      element: <Maintenance />
+    },
+    {
+      path: '/500',
+      element: <ServerErrorPage />
+    },
+    {
+      path: '/billing',
+      element: <Billing />
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />
+    }
+  ])
+
+  return (
+    <div className="w-full font-inter">
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+createRoot(document.getElementById('app') as HTMLElement).render(
+  <NavbarProvider>
+    <App />
+  </NavbarProvider>
+)
