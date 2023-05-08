0c5f0cc3e5a02e65a1e70fd7b48a5f57d14f0c1f
Ace <qanhltv11@gmail.com>
2023-05-08 18:20:42 +0700

    Update Collapsed button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -115,7 +115,7 @@ const Navbar = (): JSX.Element => {
   return (
     <div className={`relative h-full bg-[#FFFFFF] flex justify-center ${isCollapsed ? 'w-10 ml-4' : 'w-64'}`}>
       <button
-        className={`absolute -right-2 top-6 bg-[#3A57E8] w-8 h-8 flex justify-center items-center rounded-full ${isCollapsed ? '-right-16' : ''}`}
+        className={`absolute -right-2 top-6 bg-[#3A57E8] w-8 h-8 flex justify-center items-center rounded-full ${isCollapsed ? '-right-14' : ''}`}
         onClick={toggleIsCollapsed}
       >
         <ArrowLeft className={isCollapsed ? 'rotate-180' : ''} />
